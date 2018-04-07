ea09b4a86d757e0b0a1bfd83697c38622ab8255c
matthewtoye <38018265+matthewtoye@users.noreply.github.com>
2018-04-07 10:46:36 -1000

    Update Ryobi_GDO200_DH.groovy
    Added light transition from on to off.

--- Ryobi_GDO200_DH.groovy
+++ Ryobi_GDO200_DH.groovy
@@ -42,30 +42,32 @@ metadata {
 
 		// UI tile definitions
 	tiles {
-  		multiAttributeTile(name: "door", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
+  	    multiAttributeTile(name: "door", type: "lighting", width: 6, height: 4, canChangeIcon: false) {
 			tileAttribute("device.switch1", key: "PRIMARY_CONTROL") {
-            			attributeState "closed", label: 'Door Closed', action: "dooropen", icon: "st.Home.home2", backgroundColor: "#ffffff", nextState: "opening"
-				attributeState "open", label: 'Door Open', action: "doorclose", icon: "st.Home.home2", backgroundColor: "#79b821", nextState: "closing"
-            			attributeState "closing", label:'Door Closing', action:"doorclose", icon:"st.Home.home2", backgroundColor:"#00a0dc", nextState:"closed"
-				attributeState "opening", label:'Door Opening', action:"dooropen", icon:"st.Home.home2", backgroundColor:"#79b821", nextState:"open"
-           		}
-            	}
-        	standardTile("button2", "device.switch2", width: 3, height: 3, canChangeIcon: false) {
-			state "off", label: 'Light Off', action: "switch.on", icon: "st.Lighting.light11", backgroundColor: "#ffffff", nextState: "on"
-			state "on", label: 'Light On', action: "switch.off", icon: "st.Lighting.light11", backgroundColor: "#79b821", nextState: "off"
+            attributeState "closed", label: 'Door Closed', action: "dooropen", icon: "st.Home.home2", backgroundColor: "#ffffff", nextState: "opening"
+			attributeState "open", label: 'Door Open', action: "doorclose", icon: "st.Home.home2", backgroundColor: "#79b821", nextState: "closing"
+            attributeState "closing", label:'Door Closing', action:"doorclose", icon:"st.Home.home2", backgroundColor:"#00a0dc", nextState:"closed"
+			attributeState "opening", label:'Door Opening', action:"dooropen", icon:"st.Home.home2", backgroundColor:"#79b821", nextState:"open"
+            }
+        }
+        standardTile("button2", "device.switch2", width: 3, height: 3, canChangeIcon: false) {
+			state "off", label: 'Light Off', action: "switch.on", icon: "st.Lighting.light11", backgroundColor: "#ffffff", nextState: "turning_on"
+            state "turning_off", label: 'Turning Off', action: "switch.off", icon: "st.Lighting.light11", backgroundColor: "#ffffff", nextState: "off"
+			state "on", label: 'Light On', action: "switch.off", icon: "st.Lighting.light11", backgroundColor: "#79b821", nextState: "turning_off"
+            state "turning_on", label: 'Turning On', action: "switch.on", icon: "st.Lighting.light11", backgroundColor: "#79b821", nextState: "on"
 		}
-        	standardTile("refresh", "device.switch", width: 3, height: 3, inactiveLabel: false, decoration: "flat") {
-            		state "default", action:"refresh", icon:"st.secondary.refresh"
-        	}
+        standardTile("refresh", "device.switch", width: 3, height: 3, inactiveLabel: false, decoration: "flat") {
+            state "default", action:"refresh", icon:"st.secondary.refresh"
+        }
 		valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat", width: 3, height: 3) {
-            		state "battery", label: 'Battery: ${currentValue}%'
-        	}
-        	valueTile("icon", "device.icon", inactiveLabel: false, decoration: "flat", width: 3, height: 3) {
-            		state "default", label: '', icon: "https://logo-png.com/logo/ryobi-logo.png"
-        	}
+            state "battery", label: 'Battery: ${currentValue}%'
+        }
+        valueTile("icon", "device.icon", inactiveLabel: false, decoration: "flat", width: 3, height: 3) {
+            state "default", label: '', icon: "https://logo-png.com/logo/ryobi-logo.png"
+        }
 		main "door"
 			details (["door","button","button2","refresh","battery","icon"])
-	}
+            }
 }
 
 def poll() {
@@ -79,40 +81,35 @@ getStatus()
 def parse(String description){
 
 	def msg = parseLanMessage(description) 
-    	if (msg.body.startsWith("status:")) {
+    if (msg.body.startsWith("status:")) {
 		def batstatus = msg.body.split(':')[3]
-    		def doorstatus = msg.body.split(':')[2]
-    		def lightstatus = msg.body.split(':')[1]
+    	def doorstatus = msg.body.split(':')[2]
+    	def lightstatus = msg.body.split(':')[1]
 		sendEvent(name: "battery", value: batstatus)
-    		if (lightstatus == "false") {
-        		log.debug "Light OFF"
-        		sendEvent(name: "switch2", value: "off")
-   		} 
-		else if (lightstatus == "true") {
-        		log.debug "Light ON"
-        		sendEvent(name: "switch2", value: "on")
-        	}
-       		if (doorstatus == "0") {
-        		log.debug "Door Closed"
-        		sendEvent(name: "switch1", value: "closed")
-   		} 
-		else if (doorstatus == "1") {
-        		log.debug "Door Open"
-        		sendEvent(name: "switch1", value: "open")
-        	} 
-		else if (doorstatus == "2") {
-        		log.debug "Door Closing"
-        		sendEvent(name: "switch1", value: "closing")
-        	} 
-		else if (doorstatus == "3") {
-        		log.debug "Door Opening"
-        		sendEvent(name: "switch1", value: "opening")
-        	}
-    	}
+    	if (lightstatus == "false") {
+        log.debug "Light OFF"
+        sendEvent(name: "switch2", value: "off")
+   		} else if (lightstatus == "true") {
+        log.debug "Light ON"
+        sendEvent(name: "switch2", value: "on")
+        }
+       	if (doorstatus == "0") {
+        log.debug "Door Closed"
+        sendEvent(name: "switch1", value: "closed")
+   		} else if (doorstatus == "1") {
+        log.debug "Door Open"
+        sendEvent(name: "switch1", value: "open")
+        } else if (doorstatus == "2") {
+        log.debug "Door Closing"
+        sendEvent(name: "switch1", value: "closing")
+        } else if (doorstatus == "3") {
+        log.debug "Door Opening"
+        sendEvent(name: "switch1", value: "opening")
+        }
+    }
 }
-
 def on() {
-	def result = new physicalgraph.device.HubAction(
+def result = new physicalgraph.device.HubAction(
 				method: "GET",
 				path: "/?name=lighton&doorid=${doorid}&apikey=${apikey}&email=${email}&pass=${pass}",
 				headers: [
@@ -121,13 +118,13 @@ def on() {
 				)
      
 			sendHubCommand(result)
-			sendEvent(name: "switch2", value: "on")
-            		getStatus()
-			log.debug "Turning light ON" 
-}
+			sendEvent(name: "switch2", value: "turning_on")
+            log.debug "Turning light ON"
+            getStatus()
+            }
 
 def off() {
-	def result = new physicalgraph.device.HubAction(
+def result = new physicalgraph.device.HubAction(
 				method: "GET",
 				path: "/?name=lightoff&doorid=${doorid}&apikey=${apikey}&email=${email}&pass=${pass}",
 				headers: [
@@ -136,13 +133,14 @@ def off() {
 				)
                 
 			sendHubCommand(result)
-			sendEvent(name: "switch2", value: "off")
-            		getStatus()
+            
+			sendEvent(name: "switch2", value: "turning_off")        
 			log.debug "Turning light OFF"
-}
+            getStatus()
+	}
     
 def dooropen() {
-	def result = new physicalgraph.device.HubAction(
+def result = new physicalgraph.device.HubAction(
 				method: "GET",
 				path: "/?name=dooropen&doorid=${doorid}&apikey=${apikey}&email=${email}&pass=${pass}",
 				headers: [
@@ -152,13 +150,13 @@ def dooropen() {
             
 			sendHubCommand(result)
 			sendEvent(name: "switch1", value: "opening")
-            		getStatus()
-            		runIn(15,getStatus)
+            getStatus()
+            runIn(15,getStatus)
 			log.debug "OPENING Garage Door" 
-}
+            }
             
 def doorclose() {
-	def result = new physicalgraph.device.HubAction(
+def result = new physicalgraph.device.HubAction(
 				method: "GET",
 				path: "/?name=doorclose&doorid=${doorid}&apikey=${apikey}&email=${email}&pass=${pass}",
 				headers: [
@@ -168,10 +166,10 @@ def doorclose() {
            
 			sendHubCommand(result)
 			sendEvent(name: "switch1", value: "closing")
-            		runIn(5,getStatus)
-            		runIn(25,getStatus)
+            runIn(5,getStatus)
+            runIn(25,getStatus)
 			log.debug "CLOSING Garage Door" 
-}
+            }
             
 def getStatus() {
 	def result = new physicalgraph.device.HubAction(
@@ -181,6 +179,6 @@ def getStatus() {
 				HOST: "${internal_ip}:${internal_port}"
 				],
 				)
-				sendHubCommand(result)
-				log.debug "Getting Status"
-}
+			sendHubCommand(result)
+			log.debug "Getting Status"
+	}
